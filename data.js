a05ca0002e2cf0c8753ac9f22e438ab92f780404
gharieb khalifa <blacksnow.soon@gmail.com>
2024-06-04 22:11:48 +0300

    deleting test case

--- data.js
+++ data.js
@@ -4,10 +4,6 @@ const clients = [
     label: "علي",
     versions: [5,6],
     url: "ali/versions/",
-  },{
-    name: "client",
-    label: "عميل جديد",
-    versions: [5,6],
-    url: "client/versions/",
+    active: true,
   }
 ]
